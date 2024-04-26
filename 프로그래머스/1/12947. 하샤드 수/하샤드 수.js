bd14a2ec3ca01e1c960d1e789293b8bde8ac82c0
Dongkyun Kim <donkim0122@gmail.com>
2024-04-26 12:16:39 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,13 +1,9 @@
 function solution(x) {
     const num = x;
-    let counts = new Array(10).fill(0);
-    while (x > 0) {
-        counts[x % 10]++;
-        x = Math.trunc(x / 10);
-    }
     let sum = 0;
-    for (let i = 1; i < counts.length; i++) {
-        sum += counts[i] * i;
+    while (x > 0) {
+        sum += x % 10;
+        x = Math.trunc(x / 10);
     }
     return num % sum == 0;
 }
