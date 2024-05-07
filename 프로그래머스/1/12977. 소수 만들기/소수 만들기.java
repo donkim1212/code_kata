11aef297921ea09969bb7f4bb483c085ef76b0d4
Dongkyun Kim <donkim0122@gmail.com>
2024-05-07 09:32:54 +0900

    [level 1] Title: 소수 만들기, Time: 0.32 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] nums) {
+        int[] checked = new int[2998]; // max 2997
+        int cur = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    cur = nums[i] + nums[j] + nums[k];
+                    if (checked[cur] == 0) checked[cur] = checkPrime(cur);
+                    if (checked[cur] == -1) continue;
+                    if (checked[cur] == 2) count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    private int checkPrime (int num) {
+        double sqrt = Math.sqrt(num);
+        if (sqrt - Math.floor(sqrt) == 0) return -1;
+        for (int i = 2; i < num - 1; i++) {
+            if (num % i == 0) return -1;
+        }
+        return 2;
+    }
+}
