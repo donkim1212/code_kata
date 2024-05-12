df33e7e11662afa18bddc0de94ba148b0a3e553e
Dongkyun Kim <donkim0122@gmail.com>
2024-05-12 15:32:45 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,38 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public int solution(int[] ingredients) {
+        int start = findFirstBreadIndex(ingredients);
+        if (start == -1) return 0;
+        return getBurgerCountSB(ingredients, start);
+    }
+    
+    private int getBurgerCountSB (int[] ingredients, int start) {
+        StringBuilder sb = new StringBuilder();
+        int burgerCount = 0;
+        sb.append(ingredients[start]);
+        for (int i = start + 1; i < ingredients.length; i++) {
+            int cur = ingredients[i];
+            if (sb.length() > 2) {
+                if (cur == 1 && sb.charAt(sb.length() - 1) == '3') {
+                    int begin = sb.length() - 3;
+                    int end = sb.length();
+                    if (sb.substring(begin, end).equals("123")) {
+                        sb.delete(begin, end);
+                        burgerCount++;
+                        continue;
+                    }
+                }
+            } 
+            sb.append(cur);
+        }
+        return burgerCount;
+    }
+    
+    private int findFirstBreadIndex (int[] ingredients) {
+        for (int i = 0; i < ingredients.length; i++) {
+            if (ingredients[i] == 1) return i;
+        }
+        return -1;
+    }
+}
