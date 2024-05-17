64b9829d1fb06de8ee0b5cd773f49465e97d1f33
Dongkyun Kim <donkim0122@gmail.com>
2024-05-17 09:20:48 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int round = 1;
+        while ((a = getNextRoundNumber(a)) != (b = getNextRoundNumber(b))) {
+            round++;
+        }
+        return round;
+    }
+    
+    private int getNextRoundNumber(int player) {
+        if (player == 1) return 1;
+        else return player / 2 + player % 2;
+    }
+}
