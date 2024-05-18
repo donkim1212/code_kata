4c135f90d3f37325b23373dc97afba81f9bdb06c
Dongkyun Kim <donkim0122@gmail.com>
2024-05-18 15:21:54 +0900

    [level 2] Title: 귤 고르기, Time: 30.68 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,28 +1,37 @@
 import java.util.HashMap;
-import java.util.Collections;
-import java.util.ArrayList;
+import java.lang.Math;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
         if (k == 1) return 1;
         HashMap<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        int max = 0;
         
         for (int i = 0; i < tangerine.length; i++) {
             int cur = tangerine[i];
             int count = counts.containsKey(cur) ? counts.get(cur) + 1 : 1;
+            if (max < count) max = count;
             counts.put(cur, count);
         }
         
-        ArrayList<Integer> list = new ArrayList<Integer>(counts.values());
-        Collections.sort(list);
+        int[] arr = new int[max + 1];
+        for (int i : counts.values()) {
+            arr[i]++;
+        }
         
         int ret = 0;
-        for (int i = list.size() - 1; i >= 0; i--) {
-            int cur = list.get(i);
-            k -= cur;
-            ret++;
-            if (k <= 0) break;
+        int index = max + 1;
+        while (k > 0) {
+            int cur = arr[--index];
+            if (cur == 0) continue;
+            if (index == 1) break;
+            int usedKinds = Math.min(Math.max(k / index, 1), cur);
+            int usedCount = usedKinds * index;
+            k -= usedCount;
+            ret += usedKinds;
         }
+        
+        if (k > 0) ret += k;
         
         return ret;
     }
