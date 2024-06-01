e835d5593adf85e9d333fe20c7499d0a9e641fd9
Dongkyun Kim <donkim0122@gmail.com>
2024-06-01 12:56:20 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 84.26 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -0,0 +1,28 @@
+import java.lang.StringBuilder;
+import java.lang.Math;
+
+class Solution {
+    StringBuilder binary = new StringBuilder();
+    public long[] solution(long[] numbers) {
+        long[] ret = new long[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            ret[i] = findNearest(numbers[i]);
+        }
+        return ret;
+    }
+    
+    private long findNearest (long number) {
+        long ret = number + 1;
+        if (number % 2 == 0) return ret;
+        int count = 0;
+        while (number % 2 != 0) {
+            count++;
+            number /= 2;
+        }
+        if (count-- == 1) return ret;
+        for (int i = 0; i < count; i++) {
+            ret += Math.pow(2, i);
+        }
+        return ret;
+    }
+}
