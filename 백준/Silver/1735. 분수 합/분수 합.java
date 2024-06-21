2989b6e99669d426ecd4401c2b8c6e7e586c18c9
Dongkyun Kim <donkim0122@gmail.com>
2024-06-21 10:08:50 +0900

    [Silver III] Title: 분수 합, Time: 148 ms, Memory: 15940 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[] args) {
+        try {
+            int a1 = readInt();
+            int b1 = readInt();
+            int a2 = readInt();
+            int b2 = readInt();
+            int denominator = b1 * b2;
+            int numerator = a1 * b2 + a2 * b1;
+            
+            int gcd = getGCD(denominator, numerator);
+            
+            numerator /= gcd;
+            denominator /= gcd;
+            
+            System.out.print(numerator + " " + denominator);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static int getGCD (int a, int b) {
+        if (a < b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        int temp;
+        while (b != 0) {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    private static int readInt() throws IOException {
+        int c, ret = 0;
+        while ((c = System.in.read()) > 47) {
+            ret = ret * 10 + c - 48;
+        }
+        if (c == 13) System.in.read();
+        return ret;
+    }
+}
