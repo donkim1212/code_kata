d0573a8e33cea87a0f0dd6092b79b2982313ec88
Dongkyun Kim <donkim0122@gmail.com>
2024-06-26 12:36:10 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,36 +1,34 @@
 import java.io.IOException;
+import java.lang.Math;
 
 public class Main {
-    private static int N, max, c, val = 0;
-    private static int[] T, P;
+	private static int N, c, val = 0;
+    private static int[] T, P, dp;
     public static void main(String[] args) throws IOException {
         readInt();
         N = val;
         T = new int[N];
         P = new int[N];
+        dp = new int[N + 1];
         for (int i = 0; i < N; i++) {
             readInt();
             T[i] = val;
             readInt();
             P[i] = val;
         }
-        findMaxPayment(0, 0);
-        System.out.println(max);
+        findMaxPayment();
+        System.out.println(Math.max(dp[N], dp[N - 1]));
     }
     
-    private static void findMaxPayment(int index, int sum) {
-        if (index >= N) {
-            if (max < sum) max = sum;
-            return;
-        }
-        if (T[index] == 1) {
-            findMaxPayment(index + T[index], sum + P[index]);
-        }
-        if (T[index] > 1) {
-            findMaxPayment(index + 1, sum); // skip today's appointment
-            if (index + T[index] <= N) {
-                findMaxPayment(index + T[index], sum + P[index]);
-            }
+    private static void findMaxPayment() {
+    	if (T[0] <= N) {
+    		dp[T[0]] = Math.max(dp[T[0]], P[0]);
+    	}
+        for (int i = 1; i < N; i++) {
+        	dp[i] = Math.max(dp[i], dp[i - 1]);
+        	if (i + T[i] <= N) {
+        		dp[i + T[i]] = Math.max(dp[i + T[i]], dp[i] + P[i]);
+        	}
         }
     }
     
